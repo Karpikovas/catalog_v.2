a8ee0b476d14aaba8fb4ed1f8fbcdc77b8ca97b7
karpikovas <karpikovasophie@gmail.com>
2019-10-15 15:00:59 +0000

    init

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="app">
+    <div class="main">
+      <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+          <a class="navbar-brand" href="/">
+            <i class="fas fa-bookmark"></i>
+            Справочник
+          </a>
+
+          <div class="collapse navbar-collapse">
+
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <button class="btn btn-pill btn-success my-2 my-sm-0" type="submit">Добавить сотрудника</button>
+              </li>
+            </ul>
+
+          </div>
+
+        </nav>
+      </header>
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "App",
+    mounted() {
+      console.log(this.$children)
+    }
+  }
+</script>
+
+<style lang="less">
+* {
+  margin: 0;
+  padding: 0;
+}
+
+html, body {
+  margin: 0;
+  padding: 0;
+
+  min-width: 100%;
+  width: 100%;
+  max-width: 100%;
+
+  min-height: 100%;
+  height: 100%;
+  max-height: 100%;
+
+  background-color: #f5f5f5;
+}
+
+.fa-bookmark {
+  color: #42b983;
+}
+
+.main {
+  width: 100%;
+  height: 100%;
+  background-color: #f5f5f5;
+}
+.navbar-brand {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-weight: bold;
+}
+button.btn-pill {
+  border-radius: 20px;
+}
+</style>
---
+++ src/components/Employees.vue
@@ -0,0 +1,96 @@
+<template>
+    <div class="container-fluid main">
+
+        <!--------------------- Спиннер ------------------------>
+
+        <template v-if="inProgress">
+            <div class="d-flex justify-content-center mb-3">
+                <div class="spinner-border text-success"></div>
+            </div>
+        </template>
+
+        <template v-else>
+            <div class="row">
+
+        <!--------------------- Колонка с подразделениями  -------------------->
+
+                <div class="col-2 position-fixed divider">
+                    <ul class="nav justify-content-center text-center">
+                        <li class="nav-item col mb-3">
+                            Подразделения
+                        </li>
+                        <li v-for="subdivision in subdivisions" class="nav-item col mb-3">
+                            {{ subdivision.name }}
+                        </li>
+                        <li class="nav-item col mb-3">
+                            <button class="btn btn-outline-primary">Подробнее о подразделениях</button>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="col offset-md-2">
+                    <table class="table">
+                        <thead>
+                            <th v-for="field in fields"> {{ field }}</th>
+                        </thead>
+
+                    </table>
+                </div>
+
+            </div>
+        </template>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Employees",
+        data() {
+            return {
+                inProgress: false,
+                fields: ['Сотрудник', 'Подразделение', 'Должность', 'Подробности', 'Действия'],
+                subdivisions: [ { name: 'Южное' }, { name: 'Северное' }, { name: 'Западное' }, { name: 'Восточное' }, ]
+            }
+        }
+    }
+</script>
+
+<style scoped lang="less">
+    .container-fluid {
+        padding-top: 65px;
+        min-height: 100%;
+        height:auto !important; /* cross-browser */
+        height: 100%;
+
+        .nav {
+            font-weight: bold;
+
+            li.nav-item a {
+                color: green;
+
+                &.disabled {
+                    color: dimgrey;
+                }
+            }
+
+            span {
+                border-bottom: 2px solid #dee2e6;
+                border-radius: 1px;
+            }
+
+        }
+    }
+
+    .divider {
+        border-right: 1px solid #dee2e6;
+        border-radius: 1px;
+        height: 100%;
+    }
+
+    .spinner-border {
+        position: absolute;
+        top: 40%;
+        width: 5rem;
+        height: 5rem;
+    }
+</style>
